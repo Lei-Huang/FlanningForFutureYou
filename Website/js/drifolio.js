b6a78156e7686db221a6177fb7776395e7744f75
Lei Huang <u6041747@anu.edu.au>
2017-08-26 21:55:33 +1000

    Delete the navbar fadein/fadeout feature.

--- Website/js/drifolio.js
+++ Website/js/drifolio.js
@@ -93,14 +93,14 @@ $(function() {
   $(document).ready(function(){
 
     // hide .navbar first
-    $(".navbar").hide();
+    // $(".navbar").hide();
 
     // fade in .navbar
     $(function () {
         $(window).scroll(function () {
 
                  // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 40) {
+            if ($(this).scrollTop() > -40) {
                 $('.navbar')
                 .removeClass('animated fadeOutUp')
                 .addClass('animated fadeInDown')
