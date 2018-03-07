770487fd6a67ceb6ad541860d112a00d6b20c591
Wei  Wei <u6137273@anu.edu.au>
2018-03-07 11:44:27 +1100

    updating settings.py and urls.py files, to make admin site working correctly. And also create supper user: username/password (admin/admin)

--- FutureYou/FutureYou/urls.py
+++ FutureYou/FutureYou/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from PFFU.views import *
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     #url(r'^login', search),
     #url(r'^signup', regist),
+    url(r'^admin', admin.site.urls),
     url(r'^', include('PFFU.urls')),
 ]
