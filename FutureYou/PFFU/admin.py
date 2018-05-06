41dd363f4059a0e4933f34208b9ba624cc3438e4
Jiangchuan  Luo <u6100097@anu.edu.au>
2018-05-06 18:35:52 +1000

    Regist student object in admin page

--- FutureYou/PFFU/admin.py
+++ FutureYou/PFFU/admin.py
@@ -3,9 +3,17 @@ from .models import Student
 from .models import UserProfile
 from .models import Staff
 from .models import ProgressionBar
-admin.site.register(Student)
+
 admin.site.register(UserProfile)
 admin.site.register(Staff)
 admin.site.register(ProgressionBar)
-
 # Register your models here.
+
+
+class StudentAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('studentId', 'Discipline', 'Degree')
+    list_filter = ['created']
+    search_fields = ['studentId']
+
+admin.site.register(Student,StudentAdmin)
